11fa3cefcae8829e468da4864be14b725833f8f4
CentofantiEze <ezecentofanti@gmail.com>
2024-10-23 16:33:37 +0200

    Update test script.

--- scripts/test.py
+++ scripts/test.py
@@ -6,127 +6,64 @@ from argosim.antenna_utils import *
 from argosim.plot_utils import *
 from argosim.data_utils import *
 from argosim.clean import *
+from argosim.imaging_utils import *
 
-plt.rcParams['image.cmap'] = 'afmhot'
+antenna = load_antenna_enu_txt('../configs/arrays/argos_pathfinder.enu.txt')
+# plot_antenna_arr(antenna)
+baselines = get_baselines(antenna)
+# plot_baselines(baselines, ENU=True)
 
-# Antenna parameters
-x_lim = 30000
-y_lim = 30000
-n_antenna = 3*4
-n_baselines = n_antenna*(n_antenna-1)
-radius = 30000
-alpha = 120
-
+# Multiband parameters
+bandwidth = 1000e6
+n_freqs = 11
+f = 2000e6 # 2 GHz, lambda =
 # Source tracking parameters
-source_decl = -20/180*np.pi
-array_lat = 19.1/180*np.pi
-track_time = 4
-delta_t = 5/60 # 300 segs
+source_decl = 90./180*np.pi
+array_lat = 35./180*np.pi # Heraklion latitud
+track_time = 1
+delta_t = 15/60 # 300 segs
 t_0 = -0.5
+n_times = int(track_time/delta_t)
 
-# Sky model
-nx = 512
-ny = 512
-pix_sizes = [10, 7, 10, 5]
-amplitudes = [0.25, 0.25, 0.25, 0.25]
-sky = n_source_sky((nx,ny), pix_sizes, amplitudes)
+track = uv_track_multiband(baselines, array_lat, source_decl, track_time, t_0, n_times, f, bandwidth, n_freqs)
+# plot_baselines(track)
 
-# UV parameters
-uv_dim = sky.shape[0]
+Npx = 512
+fov_size = (3., 3.) # 3 degrees FOV from 6 m parabolic dishes.
+pix_size_list = [10, 7, 7, 5, 5]
+source_intensity_list = [1, 1, 1, 1, 1]
+sky = n_source_sky((Npx, Npx), pix_size_list, source_intensity_list)
+# plot_sky(sky, fov_size)
+sky_uv = sky2uv(sky)
+# plot_sky_uv(sky_uv, fov_size)
+uv_mask, uv_sample_indices  = grid_uv_samples(track, sky_uv.shape, fov_size)
+# plot_sky_uv(uv_mask, fov_size)
+vis = sky_uv*uv_mask+0+0.j 
+# plot_sky_uv(vis, fov_size)
 
-arr = y_antenna_arr(int(n_antenna/3), radius, alpha)
-arr_center = random_antenna_arr(int(n_antenna/10), int(x_lim/5), int(y_lim/5))
-arr = np.concatenate((arr,arr_center), axis=0)
-# Get baselines
-base_arr = get_baselines(arr)
-track = uv_time_int(base_arr, array_lat, source_decl, track_time, delta_t, t_0)
-# Get uv mask
-uv_plane = get_uv_plane(track,uv_dim)
-uv_plane_mask = get_uv_mask(uv_plane)
-# Get the dirty beam
-beam = get_beam(uv_plane_mask)
-# Get the sky model FT
-sky_uv = get_sky_uv(sky)
-# Sample Fourier space
-obs_uv = get_obs_uv(sky_uv,uv_plane_mask)
-# Get observed sky
-obs_sky = get_obs_sky(obs_uv, abs=True)
+obs = uv2sky(vis)
+beam = uv2sky(uv_mask)
 
-# Plot sky model, dirty beam and observation
-fig, ax = plt.subplots(1,3,figsize=(20,5))
-im=ax[0].imshow(sky)
-plt.colorbar(im, ax=ax[0])
-plot_beam(beam, ax=ax[1],fig=fig)
-im=ax[2].imshow(obs_sky)
-plt.colorbar(im, ax=ax[2])
-ax[0].set_title('Sky model')
-ax[1].set_title('Dirty beam')
-ax[2].set_title('Observation')
+fig, ax = plt.subplots(1, 3, figsize=(12,4))
+plot_sky(sky, fov_size, ax[0], fig, 'Sky')
+plot_sky(beam, fov_size, ax[1], fig, 'Beam')
+plot_sky(obs, fov_size, ax[2], fig, 'Observation')
 plt.savefig('/home/figures/observation.pdf')
 
-# Plot antenna array, baselines and aperture synthesis
-fig, ax = plt.subplots(1,3,figsize=(20,5))
-plot_antenna_arr(arr, ax=ax[0],fig=fig)
-plot_baselines(base_arr, ax=ax[1],fig=fig)
-plot_baselines(track, n_baselines=n_baselines ,ax=ax[2],fig=fig)
-ax[0].set_title('Antenna array')
-ax[1].set_title('Baselines')
-ax[2].set_title('Aperture Synthesis')
-plt.savefig('/home/figures/antenna_array.pdf')
-
-
-#---------------#
-# CLEAN algorithm
-
-# Clean hyperparams
-max_it = 5000
-treshold = 0.001
-clean_gamma = 0.4
-clean_beam_size_px = 5
-
-
-# Clean initialisation
-i=0
-X=np.zeros(sky.shape)
-X_delta = np.zeros(sky.shape)
-Y=obs_sky.copy()
-B_norm = np.fft.ifftshift(beam/np.max(beam))
-# Get the clean beam
-B_clean = gauss_source(nx, ny, np.array([0,0]), fwhm_pix=clean_beam_size_px)
-
-x_max_list = []
-y_max_list = []
-max_val_list = []
-
-while i < max_it:
-    # Search maximum in dirty obs
-    max_val, x_max, y_max, shift_x, shift_y = find_peak(Y)
-    if max_val<treshold:
-        print('Treshold reached. Stop after {} iterations.'.format(i))
-        break
-    # Subtract beam
-    Y -= max_val*clean_gamma*shift_beam(B_norm, shift_x, shift_y)
-    # Save clean component
-    X_delta[y_max,x_max] += max_val
-    X += max_val*shift_beam(B_clean, shift_x, shift_y)
-    # Track max peaks
-    x_max_list.append(x_max)
-    y_max_list.append(y_max)
-    max_val_list.append(max_val)
-    i+=1
-
-# Add residuals
-X += Y
-
+# Clean
+I_clean, sky_model = clean_hogbom(obs, beam, 0.2, 100, 1e-3 , clean_beam_size_px=10)
 # Plot clean observation
 fig, ax = plt.subplots(1,3,figsize=(20,5))
-im=ax[0].imshow(obs_sky, vmin=0)
+im=ax[0].imshow(obs)
 plt.colorbar(im, ax=ax[0])
 ax[0].set_title('Observation')
-im=ax[1].imshow(X)
+
+im=ax[1].imshow(I_clean)
 plt.colorbar(im, ax=ax[1])
 ax[1].set_title('Clean observation')
-im=ax[2].imshow(sky,vmin=0)
+
+im=ax[2].imshow(sky)
 plt.colorbar(im, ax=ax[2])
 ax[2].set_title('Sky model')
+
 plt.savefig('/home/figures/clean_observation.pdf')
