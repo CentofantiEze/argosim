8c88caefd97877fc4f62ee449de281cdd98e3ac2
CentofantiEze <ezecentofanti@gmail.com>
2024-10-25 11:12:33 +0200

    Update test script output folder.

--- scripts/test.py
+++ scripts/test.py
@@ -8,7 +8,7 @@ from argosim.data_utils import *
 from argosim.clean import *
 from argosim.imaging_utils import *
 
-antenna = load_antenna_enu_txt('../configs/arrays/argos_pathfinder.enu.txt')
+antenna = load_antenna_enu_txt('/home/configs/arrays/argos_pathfinder.enu.txt')
 # plot_antenna_arr(antenna)
 baselines = get_baselines(antenna)
 # plot_baselines(baselines, ENU=True)
@@ -21,12 +21,19 @@ f = 2000e6 # 2 GHz, lambda =
 source_decl = 90./180*np.pi
 array_lat = 35./180*np.pi # Heraklion latitud
 track_time = 1
-delta_t = 15/60 # 300 segs
+delta_t = 15/60 # 900 segs
 t_0 = -0.5
 n_times = int(track_time/delta_t)
 
 track = uv_track_multiband(baselines, array_lat, source_decl, track_time, t_0, n_times, f, bandwidth, n_freqs)
 # plot_baselines(track)
+
+# Plot the array, baselines and uv_tracks
+fig, ax = plt.subplots(1, 3, figsize=(18,5))
+plot_antenna_arr(antenna, ax[0], fig, title='Array')
+plot_baselines(baselines, ax[1], fig, ENU=True ,title='Baselines')
+plot_baselines(track, ax[2], fig, title='UV tracks')
+plt.savefig('/workdir/array_baselines.pdf')
 
 Npx = 512
 fov_size = (3., 3.) # 3 degrees FOV from 6 m parabolic dishes.
@@ -44,26 +51,29 @@ vis = sky_uv*uv_mask+0+0.j
 obs = uv2sky(vis)
 beam = uv2sky(uv_mask)
 
-fig, ax = plt.subplots(1, 3, figsize=(12,4))
+fig, ax = plt.subplots(1, 3, figsize=(18,5))
 plot_sky(sky, fov_size, ax[0], fig, 'Sky')
 plot_sky(beam, fov_size, ax[1], fig, 'Beam')
 plot_sky(obs, fov_size, ax[2], fig, 'Observation')
-plt.savefig('/home/figures/observation.pdf')
+plt.savefig('/workdir/observation.pdf')
 
 # Clean
 I_clean, sky_model = clean_hogbom(obs, beam, 0.2, 100, 1e-3 , clean_beam_size_px=10)
 # Plot clean observation
-fig, ax = plt.subplots(1,3,figsize=(20,5))
-im=ax[0].imshow(obs)
-plt.colorbar(im, ax=ax[0])
-ax[0].set_title('Observation')
+fig, ax = plt.subplots(1,3,figsize=(18,5))
+# im=ax[0].imshow(obs)
+# plt.colorbar(im, ax=ax[0])
+# ax[0].set_title('Observation')
 
-im=ax[1].imshow(I_clean)
-plt.colorbar(im, ax=ax[1])
-ax[1].set_title('Clean observation')
+# im=ax[1].imshow(I_clean)
+# plt.colorbar(im, ax=ax[1])
+# ax[1].set_title('Clean observation')
 
-im=ax[2].imshow(sky)
-plt.colorbar(im, ax=ax[2])
-ax[2].set_title('Sky model')
+# im=ax[2].imshow(sky)
+# plt.colorbar(im, ax=ax[2])
+# ax[2].set_title('Sky model')
+plot_sky(obs, fov_size, ax[0], fig, 'Observation')
+plot_sky(I_clean, fov_size, ax[1], fig, 'Clean observation')
+plot_sky(sky, fov_size, ax[2], fig, 'Sky model')
 
-plt.savefig('/home/figures/clean_observation.pdf')
+plt.savefig('/workdir/clean_observation.pdf')
